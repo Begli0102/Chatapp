341c5e2c957500360d80986eaedd0ba58278c0a6
Begli0102 <begli.kaka@mail.ru>
2021-10-26 22:38:03 +0200

    reviewed and polished code

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text,Button,TextInput, StyleSheet,Platform, KeyboardAvoidingView} from 'react-native';
+import { View, Text, Button,TextInput, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, Bubble,InputToolbar } from 'react-native-gifted-chat';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import NetInfo from '@react-native-community/netinfo';
@@ -238,10 +238,10 @@ export default class Chat extends React.Component {
           user={{
             _id: this.state.uid,
             name
-   }}
- />
- 
-       
+      }}
+    />
+    
+          
         { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
  }
       </View>
